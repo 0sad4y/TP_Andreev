508c368d536d4d069bb9952eb5e1a3ddc79c2915
0sad4y <ilyaosadchiy02@gmail.com>
2025-11-16 19:35:37 +0300

    add db connection

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,11 +1,21 @@
 package db
 
 import (
+	"log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func f() {
-	dsn := "host=localhost user=user dbname=db password=password sslmode=disable"
+func Connect() *gorm.DB {
+	dsn := "host=localhost user=postgres password=secret dbname=mydb port=5432 sslmode=disable TimeZone=UTC"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	
+	// if err := db.AutoMigrate(&User{}); err != nil {
+	// 	log.Fatalf("auto-migrate failed: %v", err)
+	// }
+
+	return db
 }
